4ff5e030955fa4854da141596e009a3931d7e4d2
Pascal Immerzeel <pascal@immerzeelpictures.com>
2014-05-15 22:58:21 +0200

    Simplify VM settings

--- manifests/params.pp
+++ manifests/params.pp
@@ -5,16 +5,9 @@ class vim::params {
 	$source          = 'git://git@github.com/immerzeel/vimfiles.git'
 	$user            = ''
 
-	case $::osfamily {
-		'Ubuntu': {
-			# On virtual machine with Ubuntu, default to 'vagrant'.
-			if $::is_virtual {
-				$user   = 'vagrant'
-				$path   = '/home/vagrant'
-			}
-		}
-		default: {
-			fail("The ${module_name} module is not supported on an ${::osfamily} based system.")
-		}
+	# On virtual machine, default to 'vagrant'.
+	if $::is_virtual {
+		$user   = 'vagrant'
+		$path   = '/home/vagrant'
 	}
 }
