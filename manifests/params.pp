2ffa83d747e3fcec6c14dc464fe45960b68a3c00
Pascal Immerzeel <pascal@immerzeelpictures.com>
2014-05-16 23:46:33 +0200

    Change git URL

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,7 +1,7 @@
 class vim::params {
-	$bundle_source   = 'git://git@github.com/gmarik/vundle.git'
+	$bundle_source   = 'git://github.com/gmarik/vundle.git'
 	$repository_name = 'vimfiles'
-	$source          = 'git://git@github.com/immerzeel/vimfiles.git'
+	$source          = 'git://github.com/immerzeel/vimfiles.git'
 
 	# On virtual machine, default to 'vagrant'.
 	if $::is_virtual {
