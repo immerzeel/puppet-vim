4785994aa98c869a6e1acf67a2cfcc5eabb5517a
Pascal Immerzeel <pascal@immerzeelpictures.com>
2014-05-16 08:32:58 +0200

    Correct error in variable assignment
    Cannot reassign variables. Puppet doesn't like that.

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,9 +1,7 @@
 class vim::params {
 	$bundle_source   = 'git://git@github.com/gmarik/vundle.git'
-	$path            = ''
 	$repository_name = 'vimfiles'
 	$source          = 'git://git@github.com/immerzeel/vimfiles.git'
-	$user            = ''
 
 	# On virtual machine, default to 'vagrant'.
 	if $::is_virtual {
