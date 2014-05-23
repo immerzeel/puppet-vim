cbfa5f88af32a2aa0208e82740ac7eb687a4610f
Pascal Immerzeel <pascal@immerzeelpictures.com>
2014-05-23 23:26:39 +0200

    Create the bundle directory implicitly
    The directory was created by git. This made the root owner of the
    directory without

--- manifests/plugins.pp
+++ manifests/plugins.pp
@@ -1,5 +1,12 @@
 class vim::plugins inherits vim {
 	if $bundle_source !='' {
+		# Create the bundle directory and chown it for the user
+		# and group, otherwise the plugins won't install.
+		file {"${path}/${repository_name}/bundle":
+			ensure => directory,
+			owner => $user,
+			group => $group
+		}
 		# Install the Vundle plugin, which is needed to install
 		# the rest of the plugins.
 		vcsrepo {"${path}/${repository_name}/bundle/vundle":
