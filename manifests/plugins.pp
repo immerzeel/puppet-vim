280b3c81fe7fd705f92598f69b21cce9003e178c
Pascal Immerzeel <pascal@immerzeelpictures.com>
2014-05-24 10:25:36 +0200

    Notify user on manual plugin installation
    The number of plugin installations cause a exec timeout in Puppet. Have
    to do it manually.

--- manifests/plugins.pp
+++ manifests/plugins.pp
@@ -10,12 +10,16 @@ class vim::plugins inherits vim {
 		# Install the Vundle plugin, which is needed to install
 		# the rest of the plugins.
 		vcsrepo {"${path}/${repository_name}/bundle/vundle":
-			before   => Exec['install_plugins'],
+			before   => Notify['install_plugins_later'],
 			ensure   => present,
 			group    => $group,
 			owner    => $user,
 			provider => git,
 			source   => $bundle_source
+		}
+
+		notify {'install_plugins_later':
+			message => "Installation of the Vim plugins takes too long. run 'vim +BundleInstall +qall' after provisioning."
 		}
 	}
 }
